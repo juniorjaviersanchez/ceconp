f9e09f0a9956e676dbf9ab0b7697c31d264fa361
juniorjaviersanchez <juniorsc750@gmail.com>
2021-12-29 11:15:33 -0500

    Cambios en correos

--- js/EstilosPersonales.js
+++ js/EstilosPersonales.js
@@ -16,7 +16,7 @@ function toggleAccordion() {
 items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 
-$(".navbar-brand").append("<div class='modal1'><h5>Encuentranos</h5><p>Av. Universitaria 1965 - Dpto 1808 <br>(Cruce Av. Universitaria y Av. La Mar) - Puebla Libre - Lima</p><p>Calle Francisco Cabrera N° 331 - Chiclayo - Lambayeque</p><p>Mz E Lote 6 de la Urb. Los Angeles de Cayma <br> Cayma - Arequipa</p><p>Av. Sánchez Cerro N° 450 - Ofc.201 Edificio Atlas - Piura</p><h5>Llamanos</h5><p>983968463 - 952892465</p><h5>Correo</h5><p>Centro de Arbitraje - arbitraje@ceconp.com  <br> Junta de Resolución de disputa - jrd@ceconp.com<br> Centro Conciliación - conciliacion@ceconp.com</p></div>");
+$(".navbar-brand").append("<div class='modal1'><h5>Encuéntranos</h5><p>Av. Universitaria 1965 - Dpto 1808 <br>(Cruce Av. Universitaria y Av. La Mar) - Puebla Libre - Lima</p><p>Calle Francisco Cabrera N° 331 - Chiclayo - Lambayeque</p><p>Mz E Lote 6 de la Urb. Los Angeles de Cayma <br> Cayma - Arequipa</p><p>Av. Sánchez Cerro N° 450 - Ofc.201 Edificio Atlas - Piura</p><h5>Llámanos</h5><p>Horario de atención es desde las 9.00 am - 11.59 am <br> 983968463 - 952892465</p><h5>Correos<h5><p> <b>Virtual</b> <br> mesadepartesvirtual@ceconp.com <br>arbitraje@ceconp.com </p><p> <b>Procesos de conciliación</b> <br> conciliacion@ceconp.com </p> <p> <b>Resolución de disputas</b> <br> jrd@ceconp.com  </p> </div>");
 
 let footerText  =  `<p>
                   <br />
@@ -28,18 +28,17 @@ let footerText  =  `<p>
                   Av. Sánchez Cerro N° 450 - Ofc.201 Edificio Atlas - Piura <br /><br />
 
                   <b>TELÉFONO</b> <br />
+                  Horario de atención es desde las 9.00 am - 11.59 am <br>
                   983968463 - 952892465 <br /><br />
 
-                  <b>E-MAIL</b><br/> 
-                  mesadepartesvirtual@ceconp.com<br/>
-                  arbitraje@ceconp.com<br/>
-                  jrd@ceconp.com
+                  <b>CORREOS</b><br/> 
+                 <b>Virtual</b> <br> mesadepartesvirtual@ceconp.com <br>arbitraje@ceconp.com <br> <b>Procesos de conciliación</b> <br> conciliacion@ceconp.com  <br> 
+                 <b>Resolución de disputas</b> <br> jrd@ceconp.com  
+
                   <br />
                   <br />
-                  Copyright &copy;
-                  <script>
-                      document.write(new Date().getFullYear());
-                  </script>
+                  Copyright &copy; 2022
+
               </p> `;
 
 $('.copywrite-text').html(footerText);
