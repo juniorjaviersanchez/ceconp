d8aac56ab291357d8500cf9eaa2cdefaa01f42ea
jhair1996 <62028849+jhair1996@users.noreply.github.com>
2021-12-29 07:41:08 -0500

    Update EstilosPersonales.js

--- js/EstilosPersonales.js
+++ js/EstilosPersonales.js
@@ -17,7 +17,7 @@ items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 
 $(".navbar-brand").append("<div class='modal1'><h5>Encuentranos</h5><p>Av. Universitaria 1965 - Dpto 1808 <br>(Cruce Av. Universitaria y Av. La Mar) - Puebla Libre - Lima</p><p>Calle Francisco Cabrera N° 331 - Chiclayo - Lambayeque</p><p>Mz E Lote 6 de la Urb. Los Angeles de Cayma <br> Cayma - Arequipa</p><p>Av. Sánchez Cerro N° 450 - Ofc.201 Edificio Atlas - Piura</p><h5>Llamanos</h5><p>983968463 - 952892465</p><h5>Correo</h5><p>Centro de Arbitraje - arbitraje@ceconp.com  <br> Junta de Resolución de disputa - jrd@ceconp.com<br> Centro Conciliación - conciliacion@ceconp.com</p></div>");
-
+$( ".navbar-brand" ). append ("<div class = 'modal1'>  <h5> Horario de Atención </h5> <P> 09:00 A.M - 11:59 A.M</p>
 let footerText  =  `<p>
                   <br />
 
