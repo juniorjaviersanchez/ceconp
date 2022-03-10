0422a915ac21e3555ab7a3f39df0c1470ae15ec6
juniorjaviersanchez <juniorsc750@gmail.com>
2022-03-10 15:43:38 -0500

    update

--- js/EstilosPersonales.js
+++ js/EstilosPersonales.js
@@ -16,13 +16,13 @@ function toggleAccordion() {
 items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 
-$(".navbar-brand").append("<div class='modal1'><h5>Encuéntranos</h5><p>Av. Universitaria 1965 - Dpto 1808 <br>(Cruce Av. Universitaria y Av. La Mar) - Puebla Libre - Lima</p><p>Calle Francisco Cabrera N° 331 - Chiclayo - Lambayeque</p><p>Mz E Lote 6 de la Urb. Los Angeles de Cayma <br> Cayma - Arequipa</p><p>Av. Sánchez Cerro N° 450 - Ofc.201 Edificio Atlas - Piura</p><h5>Llámanos</h5><p>Horario de atención es desde las 9.00 am - 11.59 am <br> 983968463 - 952892465</p><h5>Correos<h5><p> <b>Virtual</b> <br> mesadepartesvirtual@ceconp.com <br>arbitraje@ceconp.com </p><p> <b>Procesos de conciliación</b> <br> conciliacion@ceconp.com </p> <p> <b>Resolución de disputas</b> <br> jrd@ceconp.com  </p> </div>");
+$(".navbar-brand").append("<div class='modal1'><h5>Encuéntranos</h5><p>Av. Universitaria 1965 - Dpto 1808 <br>(Cruce Av. Universitaria y Av. La Mar) - Pueblo Libre - Lima</p><p>Calle Francisco Cabrera N° 331 - Chiclayo - Lambayeque</p><p>Mz E Lote 6 de la Urb. Los Angeles de Cayma <br> Cayma - Arequipa</p><p>Av. Sánchez Cerro N° 450 - Ofc.201 Edificio Atlas - Piura</p><h5>Llámanos</h5><p>Horario de atención es desde las 9.00 am - 11.59 am <br> 983968463 - 952892465</p><h5>Correos<h5><p> <b>Virtual</b> <br> mesadepartesvirtual@ceconp.com <br>arbitraje@ceconp.com </p><p> <b>Procesos de conciliación</b> <br> conciliacion@ceconp.com </p> <p> <b>Resolución de disputas</b> <br> jrd@ceconp.com  </p> </div>");
 
 let footerText  =  `<p>
                   <br />
 
                   A.Civil - CECONP <br />
-                  Av. Universitaria 1965 - Dpto 1808 (Cruce Av. Universitaria y Av. La Mar) - Puebla Libre - Lima <br />
+                  Av. Universitaria 1965 - Dpto 1808 (Cruce Av. Universitaria y Av. La Mar) - Pueblo Libre - Lima <br />
                   Calle Francisco Cabrera N° 331 - Chiclayo - Lambayeque <br />
                   Mz E Lote 6 de la Urb. Los Angeles de Cayma - Cayma - Arequipa <br />
                   Av. Sánchez Cerro N° 450 - Ofc.201 Edificio Atlas - Piura <br /><br />
